4fb543a49e71587eba95ee588750bc8a93c76e53
CelticsAFC <albertofc.es@gmail.com>
2017-02-23 22:04:23 +0100

    Update evolutions.js

--- common/data/evolutions.js
+++ common/data/evolutions.js
@@ -2219,4 +2219,60 @@ window.evolutions = {
         evolution: 1444,
         evolvers: [  ]
     },
+    416: {
+        evolution: 1445,
+        evolvers: [  ]
+    },
+    16: {
+        evolution: 1446,
+        evolvers: [  ]
+    },
+    20: {
+        evolution: 1447,
+        evolvers: [  ]
+    },
+    22: {
+        evolution: 1448,
+        evolvers: [  ]
+    },
+    1449: {
+        evolution: 1450,
+        evolvers: [ 116, 80, 86, 91, 102 ]
+    },
+    1451: {
+        evolution: 1452,
+        evolvers: [ 117, 80, 91, 97, 102 ]
+    },
+    1453: {
+        evolution: 1454,
+        evolvers: [  ]
+    },
+    1455: {
+        evolution: 1456,
+        evolvers: [ 118, 266, 98, 303, 103 ]
+    },
+    1457: {
+        evolution: 1458,
+        evolvers: [ 83, 85, 88, 96, 99 ]
+    },
+    1459: {
+        evolution: 1460,
+        evolvers: [ 117, 78, 89, 95 ]
+    },
+    1462: {
+        evolution: 1463,
+        evolvers: [  ]
+    },
+    1464: {
+        evolution: 1465,
+        evolvers: [  ]
+    },
+    1466: {
+        evolution: 1467,
+        evolvers: [  ]
+    },
+    1468: {
+        evolution: 1469,
+        evolvers: [  ]
+    },
 };

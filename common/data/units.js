8fbce9506d4f4857bdcb47f8c2c30e857412795b
Usuario <aribeiro584@gmail.com>
2022-01-16 10:20:41 +0100

    prueba cambio nombre

--- common/data/units.js
+++ common/data/units.js
@@ -1,5 +1,5 @@
 window.units = [
-	 [ "Monkey D. Luffy", "STR", "Fighter", 2, 1, 5, 0, 5, 163, 42, 15, 8, 134, 68, 15, 1 ],
+	 [ "Monkey D. Monkey", "STR", "Fighter", 2, 1, 5, 0, 5, 163, 42, 15, 8, 134, 68, 15, 1 ],
 	 [ "Monkey D. Luffy 'Gomu Gomu no Pistol'", "STR", "Fighter", 3, 4, 6, 0, 20, 26184, 130, 76, 14, 510, 249, 49, 0.7 ],
 	 [ "Monkey D. Luffy 'Gomu Gomu no Bazooka'", "STR", "Fighter", 4, 7, 6, 2, 35, 136167, 404, 227, 39, 975, 522, 105, 1 ],
 	 [ "Monkey D. Luffy 'Gear Second'", "STR", ["Fighter", "Free Spirit"], 5, 15, 8, 3, 99, 3000000, 902, 473, 74, 1772, 1313, 227, 1 ],
